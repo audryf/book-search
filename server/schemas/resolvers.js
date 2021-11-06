00c865ec1506da45f4e036f82b23d8e8968c8174
audryf <84256033+audryf@users.noreply.github.com>
2021-11-05 20:38:17 -0500

    add schemas, typedefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,78 @@
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Please log in to continue');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, args) => {
+            const user = await User.findOne({ email: args.email });
+
+            if (!user) {
+                throw new AuthenticationError('Invalid credentials');
+            }
+
+            const valid = await user.validatePassword(args.password);
+
+            if (!valid) {
+                throw new AuthenticationError('Invalid credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('Please log in to continue');
+            }
+
+            const user = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { books: args.bookId } },
+                { new: true, runValidators: true }
+                );
+
+            await user.save();
+
+            return user;
+        },
+        removeBook: async (parent, args, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('Please log in to continue');
+            }
+
+            const user = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { books: args.bookId } },
+                { new: true, runValidators: true }
+                );
+
+            await user.save();
+
+            return user;
+        }
+    }
+}
+
+module.exports = resolvers;
