9b66142bb64b32aae35c8d2a6d249f8e03b54c04
audryf <84256033+audryf@users.noreply.github.com>
2021-11-05 19:54:06 -0500

    initial file uplaod, update server.js

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  // start server
+  await server.start();
+
+  //integrate apollo server with express as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initilaize the apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes); // this will not be here when we switch to graphql
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
